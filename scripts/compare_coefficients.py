43bf6f1265db0de1c35020becd7c68229869efb6
MiriamAlina <miriam.goldack@outlook.com>
2026-03-04 16:16:45 +0100

    style: clean up file
    Signed-off-by: MiriamAlina <miriam.goldack@outlook.com>

--- scripts/compare_coefficients.py
+++ scripts/compare_coefficients.py
@@ -4,7 +4,7 @@ from sklearn.metrics import (
     r2_score,
     mean_squared_error,
     mean_absolute_error,
-    # root_mean_squared_error
+    root_mean_squared_error
     )
 from src.cosine_similarity import cosine_similarity
 from src.aft import compute_AFT_solution
@@ -44,13 +44,11 @@ def compare_coefficients(y_true, y_pred, normalize=False, eps=1e-14):
                                     multioutput='uniform_average')
     mae_global = mean_absolute_error(y_true, y_pred,
                                      multioutput='uniform_average')
-    rmse_global = np.sqrt(mse_global)
+    rmse_global = root_mean_squared_error(y_true, y_pred,
+                                          multioutput='uniform_average')
 
     # cosine similarity over whole dataset (flattened vectors)
-    cos_global = cosine_similarity(
-        y_true.ravel(),
-        y_pred.ravel()
-    )
+    cos_global = cosine_similarity(y_true.ravel(), y_pred.ravel())
 
     # ---- Per-output metrics ----
     rel_l2_per_mode = []
@@ -67,7 +65,7 @@ def compare_coefficients(y_true, y_pred, normalize=False, eps=1e-14):
         r2_per.append(r2_score(y_true[:, i], y_pred[:, i]))
         mse_i = mean_squared_error(y_true[:, i], y_pred[:, i])
         mae_i = mean_absolute_error(y_true[:, i], y_pred[:, i])
-        rmse_i = np.sqrt(mse_i)
+        rmse_i = root_mean_squared_error(y_true[:, i], y_pred[:, i])
 
         cos_i = cosine_similarity(
             y_true[:, i].ravel(),
@@ -158,111 +156,80 @@ def plot_radar(scores):
     plt.show()
 
 
-# training data
-training_data_id = '2026-02-18_14-04-47'
-training_data = np.load('data/duffing_training_data_H3_N64_' +
-                        training_data_id + '.npz')
-q_train = training_data['q_coeffs']
-fnl_train_aft = training_data['fnl_coeffs']
+# test data
+test_data_id = '2026-02-18_14-04-47'
+test_data = np.load('data/duffing_test_data_H3_N64_' +
+                    test_data_id + '.npz')
+q_test = test_data['q_coeffs']
+fnl_test_aft = test_data['fnl_coeffs']
 
-# NN performance on training data
+# NN performance on test data
 nn_id = '2026-02-18_13-29-30'  # identifier of the trained NN model
-fnl_train_nn = []
-for i in range(len(q_train)):
-    q_input = np.concatenate([q_train[i, :]])
-    fnl_train_nn.append(evaluate_Duffing_nn_H3(nn_id, q_input))
-fnl_train_nn = np.array(fnl_train_nn)
-
+fnl_test_nn = []
+for i in range(len(q_test)):
+    q_input = np.concatenate([q_test[i, :]])
+    fnl_test_nn.append(evaluate_Duffing_nn_H3(nn_id, q_input))
+fnl_test_nn = np.array(fnl_test_nn)
 
 # physically relevant inputs over FRC iterations
-q_rel = np.loadtxt('data/nn_input_Duffing.txt', delimiter=',')
+q_rel_full = np.loadtxt('data/nn_input_Duffing.txt', delimiter=',')
 
 # NN performance on FRC relevant input trajectory
 H = 3
 N = 2**6
 gamma = 0.1
-fnl_rel_aft = []
-fnl_rel_nn = []
-for i in range(np.shape(q_rel)[0]):
-    q_ce = convert_cossin_to_comexp(q_rel[i, :7])
+fnl_rel_aft = np.empty((0, 4))
+fnl_rel_nn = np.empty((0, 4))
+q_rel = np.empty((0, 4))
+for i in range(np.shape(q_rel_full)[0]):
+    q_ce = convert_cossin_to_comexp(q_rel_full[i, :7])
+    q_cs = np.concatenate([q_rel_full[i, 1:3], q_rel_full[i, 5:7]])
+    q_rel = np.vstack([q_rel, q_cs])
     fnl_ce = compute_AFT_solution(N, H, q_ce, gamma)
     fnl_cs = convert_comexp_to_cossin(fnl_ce, H)
     fnl_cs_NN = evaluate_Duffing_nn_H3(
-        nn_id, np.concatenate([q_rel[i, 1:3], q_rel[i, 5:7]]))
-    fnl_rel_aft.append(fnl_cs)
-    fnl_rel_nn.append(fnl_cs_NN)
+        nn_id, q_cs)
+    fnl_rel_aft = np.vstack([fnl_rel_aft, fnl_cs[[1, 2, 5, 6]]])
+    fnl_rel_nn = np.vstack([fnl_rel_nn, fnl_cs_NN])
 
-fnl_rel_aft = np.array(fnl_rel_aft)
-fnl_rel_aft_comparison = fnl_rel_aft[:, [1, 2, 5, 6]]
-fnl_rel_nn = np.array(fnl_rel_nn)
-metrics = compare_coefficients(fnl_rel_aft_comparison, fnl_rel_nn)
+metrics = compare_coefficients(fnl_rel_aft, fnl_rel_nn)
 radar_scores = metrics_to_radar_scores(metrics)
 plot_radar(radar_scores)
 
 
-# NN prediction vs. AFT ground-truth over training samples and FRC inputs
-fig, ax = plt.subplots(2, 2, figsize=(7, 8))
-ax[0, 0].plot(fnl_train_aft[:, 0], fnl_train_nn[:, 0], 'o', label='A1',
-              color='#A8dadc')
-ax[0, 0].plot(fnl_rel_aft[:, 1], fnl_rel_nn[:, 0], 'x',
-              label='A1 (rel)', color='#E63946')
-ax[0, 0].set_xlabel('A1 true')
-ax[0, 0].set_ylabel('A1 predicted')
-ax[0, 0].legend()
-ax[0, 1].plot(fnl_train_aft[:, 1], fnl_train_nn[:, 1], 'o', label='B1',
-              color='#A8dadc')
-ax[0, 1].plot(fnl_rel_aft[:, 2], fnl_rel_nn[:, 1], 'x',
-              label='B1 (rel)', color='#E63946')
-ax[0, 1].set_xlabel('B1 true')
-ax[0, 1].set_ylabel('B1 predicted')
-ax[0, 1].legend()
-ax[1, 0].plot(fnl_train_aft[:, 2], fnl_train_nn[:, 2], 'o', label='A3',
-              color='#A8dadc')
-ax[1, 0].plot(fnl_rel_aft[:, 5], fnl_rel_nn[:, 2], 'x',
-              label='A3 (rel)', color='#E63946')
-ax[1, 0].set_xlabel('A3 true')
-ax[1, 0].set_ylabel('A3 predicted')
-ax[1, 0].legend()
-ax[1, 1].plot(fnl_train_aft[:, 3], fnl_train_nn[:, 3], 'o', label='B3',
-              color='#A8dadc')
-ax[1, 1].plot(fnl_rel_aft[:, 6], fnl_rel_nn[:, 3], 'x',
-              label='B3 (rel)', color='#E63946')
-ax[1, 1].set_xlabel('B3 true')
-ax[1, 1].set_ylabel('B3 predicted')
-ax[1, 1].legend()
+colors = ['#1D3557', '#008b9a', '#f19699', '#e63946']
+q_labels = ['A1', 'B1', 'A3', 'B3']
+fnl_labels = ['A1', 'B1', 'A3', 'B3']
+
+# NN prediction vs. AFT ground-truth over test samples and FRC inputs
+fig, ax = plt.subplots(1, 4, figsize=(15, 4))
+for i in range(4):
+    ax[i].plot(fnl_test_aft[:, i], fnl_test_nn[:, i], 'o', label=fnl_labels[i],
+               color='#A8DADC')
+    ax[i].plot(fnl_rel_aft[:, i], fnl_rel_nn[:, i], 'x',
+               label=f'{fnl_labels[i]} (rel)', color='#E63946')
+    ax[i].set_xlabel(f'{fnl_labels[i]} true')
+    ax[i].set_ylabel(f'{fnl_labels[i]} predicted')
+    ax[i].legend()
 plt.suptitle(f'ground truth vs. prediction for dataset {nn_id}')
 plt.tight_layout()
 
 
 # AFT vs. NN over FRC iterations
-colors = ['#1D3557', '#008b9a', '#f19699', '#e63946']
 fig, ax = plt.subplots(4, 1, figsize=(5, 8))
-ax[0].plot(q_rel[:, 1], label='a1', color=colors[0])
-ax[0].plot(q_rel[:, 2], label='b1', color=colors[1])
-ax[0].plot(q_rel[:, 5], label='a3', color=colors[2])
-ax[0].plot(q_rel[:, 6], label='b3', color=colors[3])
+for i in range(4):
+    ax[0].plot(q_rel[:, i], label=q_labels[i], color=colors[i])
+    ax[1].plot(fnl_rel_aft[:, i], label=fnl_labels[i],
+               color=colors[i])
+    ax[2].plot(fnl_rel_nn[:, i], label=f'{fnl_labels[i]}_NN', color=colors[i])
+    ax[3].plot(fnl_rel_aft[:, i] - fnl_rel_nn[:, i],
+               label=f'{fnl_labels[i]} error', color=colors[i])
 ax[0].legend()
 ax[0].set_title('Input coefficients over iterations')
-ax[1].plot(fnl_rel_aft[:, 1], label='A1', color=colors[0])
-ax[1].plot(fnl_rel_aft[:, 2], label='B1', color=colors[1])
-ax[1].plot(fnl_rel_aft[:, 5], label='A3', color=colors[2])
-ax[1].plot(fnl_rel_aft[:, 6], label='B3', color=colors[3])
 ax[1].legend()
 ax[1].set_title('AFT output coefficients over iterations')
-ax[2].plot(fnl_rel_nn[:, 0], label='A1_NN', color=colors[0])
-ax[2].plot(fnl_rel_nn[:, 1], label='B1_NN', color=colors[1])
-ax[2].plot(fnl_rel_nn[:, 2], label='A3_NN', color=colors[2])
-ax[2].plot(fnl_rel_nn[:, 3], label='B3_NN', color=colors[3])
 ax[2].legend()
 ax[2].set_title('NN output coefficients over iterations')
-ax[3].plot(fnl_rel_aft[:, 1]-fnl_rel_nn[:, 0], label='A1 error',
-           color=colors[0])
-ax[3].plot(fnl_rel_aft[:, 2]-fnl_rel_nn[:, 1], label='B1 error',
-           color=colors[1])
-ax[3].plot(fnl_rel_aft[:, 5]-fnl_rel_nn[:, 2], label='A3 error',
-           color=colors[2])
-ax[3].plot(fnl_rel_aft[:, 6]-fnl_rel_nn[:, 3], label='B3 error',
-           color=colors[3])
 ax[3].legend()
 ax[3].set_title('Error between AFT and NN outputs over iterations')
 plt.tight_layout()
